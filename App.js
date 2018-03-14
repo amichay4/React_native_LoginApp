1c6f7c96748b5ad597c78f8436e4b7f1208d191a
amichay4 <einhorn.amichay@gmail.com>
2018-03-14 20:17:49 +0200

    Update App.js

--- App.js
+++ App.js
@@ -2,13 +2,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { FBLogin, FBLoginManager } from 'react-native-facebook-login';
 
-class Login extends React.Component {
-  render() {
-    return (
-      <FBLogin />
-    );
-  }
-};
+
 
 export default class App extends React.Component {
   render() {
