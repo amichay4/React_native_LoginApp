dd76ac4f9e1f429aaeb9b5b73bd32d3f561df12d
amichay4 <einhorn.amichay@gmail.com>
2018-03-18 09:51:08 +0200

    Add files via upload

--- App.js
+++ App.js
@@ -1,23 +1,184 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import firebase from 'react-native-firebase';
+import { StyleSheet, Platform, Image, Linking, Text, View } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { AccessToken, LoginManager } from 'react-native-fbsdk';
+import { GoogleSignin } from 'react-native-google-signin';
+
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: true,
+    };
+  }
+  
+  componentDidMount() {
+    this.authSubscription = firebase.auth().onAuthStateChanged((user) => {
+      this.setState({
+        loading: false,
+        user,
+      });
+    });
+  }
+  /**
+   * Don't forget to stop listening for authentication state changes
+   * when the component unmounts.
+   */
+  componentWillUnmount() {
+    this.authSubscription();
+
+  // Handle Login with Facebook button tap
+  loginWithFacebook = () =>{
+      LoginManager.logInWithReadPermissions(['public_profile', 'email'])
+        .then((result) => {
+          if (result.isCancelled) {
+            return Promise.reject(new Error('The user cancelled the request'));
+          }
+          // Retrieve the access token
+          return AccessToken.getCurrentAccessToken();
+        })
+        .then((data) => {
+          // Create a new Firebase credential with the token
+          const credential = firebase.auth.FacebookAuthProvider.credential(data.accessToken);
+          // Login with the credential
+          return firebase.auth().signInWithCredential(credential);
+        })
+        .then((user) => {
+          // If you need to do anything with the user, do it here
+          // The user will be logged in automatically by the
+          // `onAuthStateChanged` listener we set up in App.js earlier
+        })
+        .catch((error) => {
+          const { code, message } = error;
+          // For details of error codes, see the docs
+          // The message contains the default Firebase string
+          // representation of the error
+        });
+    }
+  } ;
+  
+
+  // Handle Login with Google button tap
+  loginWithGoogle = () => {
+    GoogleSignin.signIn()
+    .then((data) => {
+      // Create a new Firebase credential with the token
+      const credential = firebase.auth.GoogleAuthProvider.credential(data.idToken, data.accessToken);
+      // Login with the credential
+      return firebase.auth().signInWithCredential(credential);
+    })
+    .then((user) => {
+      // If you need to do anything with the user, do it here
+      // The user will be logged in automatically by the
+      // `onAuthStateChanged` listener we set up in App.js earlier
+    })
+    .catch((error) => {
+      const { code, message } = error;
+      // For details of error codes, see the docs
+      // The message contains the default Firebase string
+      // representation of the error
+    });
+  };
+
+  
+
   render() {
+    const { user } = (this.state.user); 
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-        <Login />
+        {  (this.state.user)
+          ? // Show user info if already logged in
+          <View style={styles.content}>
+            <Text style={styles.header}>
+              Welcome {firebase.auth().currentUser.name}!
+            </Text>
+            <View style={styles.avatar}>
+              <Image source={{ uri: firebase.auth().currentUser.avatar }} style={styles.avatarImage} />
+            </View>
+        </View>
+          : // Show Please log in message if not
+          <View style={styles.content}>
+            <Text style={styles.header}>
+              Welcome Stranger!
+            </Text>
+            <View style={styles.avatar}>
+              <Icon name="user-circle" size={100} color="rgba(0,0,0,.09)" />
+            </View>
+            <Text style={styles.text}>
+              Please log in to continue {'\n'}
+              to the awesomness
+            </Text>
+        </View>
+        }
+  
+        {/* Login buttons */}
+        <View style={styles.buttons}>
+          <Icon.Button
+            name="facebook"
+            backgroundColor="#3b5998"
+            onPress={this.loginWithFacebook}
+            {...iconStyles}
+          >
+            Login with Facebook
+          </Icon.Button>
+          <Icon.Button
+            name="google"
+            backgroundColor="#DD4B39"
+            onPress={this.loginWithGoogle}
+            {...iconStyles}
+          >
+            Or with Google
+          </Icon.Button>
+        </View>
       </View>
     );
   }
+
 }
+
+const iconStyles = {
+  borderRadius: 10,
+  iconStyle: { paddingVertical: 5 },
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#FFF',
+  },
+  content: {
+    flex: 1,
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: 20,
+  },
+  avatarImage: {
+    borderRadius: 50,
+    height: 100,
+    width: 100,
+  },
+  header: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  text: {
+    textAlign: 'center',
+    color: '#333',
+    marginBottom: 5,
+  },
+  buttons: {
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    margin: 20,
+    marginBottom: 30,
   },
 });
+
+
+
+
